0c3a442702874ea9d02f91c02a909f63eef5f2a9
zatserkl <zatserkl@gmail.com>
2023-04-26 12:41:53 -0400

    first commit

---
+++ helloworld/shift.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+my @a = (1..5);
+print "original: \@a: @a\n";
+
+while (@a) {
+    print "\@a: @a\n";
+    shift @a;
+}
+
+print "\nunshift\n\n";
+
+my @b = (1..5);
+print "original: \@b: @b\n";
+unshift(@b, 10);
+print "after unshift(\@b, 10): @b\n";
+
+print "\nCompare with push\n\n";
+
+my @c = (1..5);
+print "original: \@c: @c\n";
+push(@c, 10);
+print "after push(\@c, 10): @c\n";
